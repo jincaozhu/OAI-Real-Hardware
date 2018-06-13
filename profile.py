a8b077e3ba9d2dbaca6910707013e213cc03d691
jincaozhu <33697973+jincaozhu@users.noreply.github.com>
2018-06-12 20:28:00 -0600

    Update profile.py

--- profile.py
+++ profile.py
@@ -166,7 +166,7 @@ else:
     enb2.Desire( "rf-radiated" if params.TYPE == "ota" else "rf-controlled", 1 )
     connectOAI_DS(enb2, 0)
     enb2.addService(rspec.Execute(shell="sh", command=GLOBALS.OAI_CONF_SCRIPT + " -r ENB"))
-    enb2_nfapi_if = enb1.addInterface("enb2_nfapi")
+    enb2_nfapi_if = enb2.addInterface("enb2_nfapi")
     enb2_nfapi_if.addAddress( rspec.IPv4Address( "192.168.10.2", "255.255.255.0" ) ) # nfapi interface
 	
     nfapilink = request.Link( "nfapi" )
